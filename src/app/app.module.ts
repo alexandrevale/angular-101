39a2b16fefa8709dd95e6c64e948dc4c43cf8720
Alexandre do Vale <vale.apv@gmail.com>
2019-02-24 13:41:35 -0300

    Meu primeiro component
    Criação e integração do component com a aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-
+import { MeuPrimeiroComponent } from "./meu-primeiro/meu-primeiro.component";
+// from "meu-primeiro/meu-primeiro.component";
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MeuPrimeiroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
